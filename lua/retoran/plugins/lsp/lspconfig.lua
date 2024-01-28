f68fcbc3827e9ca680ad1078a387ec8bb47d6593
RetoranPetra <flyro@live.co.uk>
2024-01-28 13:48:00 +0000

    Automatic LSP setup with mason

--- lua/retoran/plugins/lsp/lspconfig.lua
+++ lua/retoran/plugins/lsp/lspconfig.lua
@@ -4,5 +4,23 @@ return {
 		"williamboman/mason-lspconfig.nvim",
 		dependencies = "williamboman/mason.nvim"
 	},
-	opts = {}
+	opts = {},
+	config = function(_, opts)
+		-- Use masonlspconfig to automatically setup our servers.
+		-- See :h mason-lspconfig-automatic-server-setup
+    require("mason-lspconfig").setup_handlers {
+        -- The first entry (without a key) will be the default handler
+        -- and will be called for each installed server that doesn't have
+        -- a dedicated handler.
+        function (server_name) -- default handler (optional)
+            require("lspconfig")[server_name].setup {}
+        end,
+        -- Next, you can provide a dedicated handler for specific servers.
+        -- For example, a handler override for the `rust_analyzer`:
+        --["rust_analyzer"] = function ()
+        --    require("rust-tools").setup {}
+        --end
+    }
+		
+	end
 }
