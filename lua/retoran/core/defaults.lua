9702bba3f2652c4d9801a477e48c2dc0d24b236b
RetoranPetra <flyro@live.co.uk>
2024-01-28 12:45:22 +0000

    Auto nvim tree enter

--- lua/retoran/core/defaults.lua
+++ lua/retoran/core/defaults.lua
@@ -1,9 +1,4 @@
 local opt = vim.opt
--- Disable netrw so neotree can be used
-
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-
 opt.timeoutlen = 300
 opt.undofile = true
 opt.undolevels = 10000
