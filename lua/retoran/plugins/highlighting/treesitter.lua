5229a14e6fbcd8771dffe690b64af041ba9109b0
RetoranPetra <flyro@live.co.uk>
2024-01-28 15:14:33 +0000

    Updated treesitter to sync install.

--- lua/retoran/plugins/highlighting/treesitter.lua
+++ lua/retoran/plugins/highlighting/treesitter.lua
@@ -3,7 +3,9 @@ return {
 	opts = {
 		auto_install = true,
 		highlight = { enable = true},
-		indent = { enable = true }
+		indent = { enable = true },
+		-- Don't install all at once, better for windows.
+		sync_install = true
 	},
 	build = function ()
 		require("nvim-treesitter.install").update({ with_sync = true })()
