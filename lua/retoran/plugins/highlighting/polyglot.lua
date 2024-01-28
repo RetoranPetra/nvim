88ff86417e4a3bbb386654493c7cbe6047f8ee89
RetoranPetra <flyro@live.co.uk>
2024-01-28 15:24:13 +0000

    Added polyglot (may want to remove)

---
+++ lua/retoran/plugins/highlighting/polyglot.lua
@@ -0,0 +1,15 @@
+return {
+	"sheerun/vim-polyglot",
+	-- Quick and dirty solution to make polyglot not use parsers intalled by treesitter.
+	init = function()
+		local parsers = require("nvim-treesitter.info").installed_parsers()
+		for i = 1, #parsers do
+			parsers[i] = parsers[i] .. ".plugin"
+		end
+		table.insert(parsers, 1, "sensible")
+		vim.g.polyglot_disabled = parsers
+	end,
+	event = "bufEnter",
+	config = true
+}
+
