0fcd5873f020863c25fc054ecde571cebe82ae80
RetoranPetra <flyro@live.co.uk>
2024-02-14 16:00:35 +0000

    Auto-Install mason tools as needed

--- lua/retoran/plugins/lsp/mason.lua
+++ lua/retoran/plugins/lsp/mason.lua
@@ -6,5 +6,26 @@ return {
 			automatic_installation = false
 		}
 	},
-	{ "williamboman/mason.nvim", config = true}
+	{
+		"williamboman/mason.nvim",
+		opts = {},
+	},
+	{
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		opts = {
+			ensure_installed = {
+				"lua-language-server",
+				"omnisharp",
+				"clangd",
+				"rust-analyzer",
+				"tree-sitter-cli"
+			},
+			auto_update = true,
+			run_on_start = true,
+			debounce_hours = 12
+		},
+		dependencies = {
+			"williamboman/mason.nvim",
+		},
+	},
 }
