076075910cd242311f45d53e8309da08d8a8a891
RetoranPetra <flyro@live.co.uk>
2024-02-14 16:42:14 +0000

    Bindings for lazygit

--- lua/retoran/plugins/lazygit.lua
+++ lua/retoran/plugins/lazygit.lua
@@ -1,4 +1,7 @@
 return {
 	"kdheepak/lazygit.nvim",
-	dependencies = {"nvim-lua/plenary.nvim"}
+	dependencies = {"nvim-lua/plenary.nvim"},
+	config = function ()
+		vim.keymap.set("n", "<leader>gg", ":LazyGit<CR>")
+	end
 }
