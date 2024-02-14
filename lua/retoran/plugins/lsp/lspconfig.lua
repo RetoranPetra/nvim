fc50c6d6a31b7d0c9f6c5b583eba05b308a87416
RetoranPetra <flyro@live.co.uk>
2024-02-14 16:41:33 +0000

    Gave neodev empty opts to init

--- lua/retoran/plugins/lsp/lspconfig.lua
+++ lua/retoran/plugins/lsp/lspconfig.lua
@@ -6,7 +6,7 @@ return {
 			"williamboman/mason-lspconfig.nvim",
 			dependencies = "williamboman/mason.nvim"
 		},
-		"folke/neodev.nvim"
+		{ "folke/neodev.nvim", opts = {} }
 	},
 	event = "BufEnter",
 	opts = function(_, opts)
