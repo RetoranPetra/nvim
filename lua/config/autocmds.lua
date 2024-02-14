3b0bd718d9a8d183fe5eda7bb172434c5f8664e6
RetoranPetra <flyro@live.co.uk>
2024-02-14 16:41:25 +0000

    Mason external update + omnisharp

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -8,14 +8,14 @@ vim.api.nvim_clear_autocmds({ group = "lazyvim_highlight_yank" })
 -- Override nightfly highlights.
 local custom_highlight = vim.api.nvim_create_augroup("CustomHighlight", {})
 vim.api.nvim_create_autocmd("ColorScheme", {
-  pattern = "nightfly",
-  callback = function()
-  if vim.g.usingGUI == nil then
-    vim.api.nvim_set_hl(0, "NeoTreeNormal", { bg = "NONE", fg = "#c3ccdc" })
-    vim.api.nvim_set_hl(0, "NeoTreeNormalNC", { bg = "NONE", fg = "#c3ccdc" })
-  end
-  end,
-  group = custom_highlight,
+	pattern = "nightfly",
+	callback = function()
+		if vim.g.usingGUI == nil then
+			vim.api.nvim_set_hl(0, "NeoTreeNormal", { bg = "NONE", fg = "#c3ccdc" })
+			vim.api.nvim_set_hl(0, "NeoTreeNormalNC", { bg = "NONE", fg = "#c3ccdc" })
+		end
+	end,
+	group = custom_highlight,
 })
 
 -- User overrides for autocmds, need this func for it.
