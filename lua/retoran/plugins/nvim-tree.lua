c11104d3ae4fb8e0e73cb4e02589e1144c24f3ae
RetoranPetra <flyro@live.co.uk>
2024-02-14 16:41:27 +0000

    nvim tree working

--- lua/retoran/plugins/nvim-tree.lua
+++ lua/retoran/plugins/nvim-tree.lua
@@ -2,5 +2,10 @@ return {
 	"nvim-tree/nvim-tree.lua",
 	dependencies = {
 		"nvim-tree/nvim-web-devicons"
-	}
+	},
+	init = function() 
+		vim.g.loaded_netrw = 1
+		vim.g.loaded_netrwPlugin = 1
+	end,
+	config = true
 }
