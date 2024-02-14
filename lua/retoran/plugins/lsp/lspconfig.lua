e08d00daca8bc4b090d5060e02efceb5442f11f1
RetoranPetra <flyro@live.co.uk>
2024-02-14 16:41:27 +0000

    Moved mason to its own file

--- lua/retoran/plugins/lsp/lspconfig.lua
+++ lua/retoran/plugins/lsp/lspconfig.lua
@@ -1,18 +1,8 @@
 return {
-	{
-		"neovim/nvim-lspconfig",
-		opts = {},
-		dependencies = {
-			"williamboman/mason-lspconfig.nvim",
-			opts = {
-				automatic_installation = true
-			},
-			dependencies = {
-				{
-					"williamboman/mason.nvim",
-					opts = {}
-				}
-			}
-		}
-	}
+	"neovim/nvim-lspconfig",
+	dependencies = {
+		"williamboman/mason-lspconfig.nvim",
+		dependencies = "williamboman/mason.nvim"
+	},
+	opts = {}
 }
