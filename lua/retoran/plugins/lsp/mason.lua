cdf95e74807588bf64505afd833e4796a5ac2990
RetoranPetra <flyro@live.co.uk>
2024-02-14 16:42:11 +0000

    Moved mason to its own file

---
+++ lua/retoran/plugins/lsp/mason.lua
@@ -0,0 +1,10 @@
+return {
+	{
+		"williamboman/mason-lspconfig.nvim",
+		dependencies = "williamboman/mason.nvim",
+		opts = {
+			automatic_installation = false
+		}
+	},
+	{ "williamboman/mason.nvim", config = true}
+}
