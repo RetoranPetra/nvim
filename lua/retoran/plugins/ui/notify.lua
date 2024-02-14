d7890be28a0d333436efcd4f546fb1a9be0d344b
RetoranPetra <flyro@live.co.uk>
2024-02-14 16:41:35 +0000

    doc: Added todo in nvim-notify

--- lua/retoran/plugins/ui/notify.lua
+++ lua/retoran/plugins/ui/notify.lua
@@ -1,5 +1,6 @@
 return {
 	"rcarriga/nvim-notify",
+	-- TODO: Add a way to zoom around the notifications, and a binding to clear them quickly.
 	opts = {
 		background_colour = "#000000",
 		render = "wrapped-compact",
