d9fbb3477b0e16b54858971c811cd76f2e784b0b
RetoranPetra <flyro@live.co.uk>
2024-02-14 16:00:31 +0000

    Auto nvim tree enter

--- lua/retoran/plugins/nvim-tree.lua
+++ lua/retoran/plugins/nvim-tree.lua
@@ -7,5 +7,14 @@ return {
 		vim.g.loaded_netrw = 1
 		vim.g.loaded_netrwPlugin = 1
 	end,
-	config = true
+	opts = {
+	},
+	-- Takes opts from above.
+	config = function(_,opts)
+		require("nvim-tree").setup(opts)
+		-- Opens neotree on startup no matter what.
+		require("nvim-tree.api").tree.open()
+		 -- Ideally want to keep focus on our buffer when we open neo tree, so probably want to change this.
+	end,
+	event = "VimEnter"
 }
