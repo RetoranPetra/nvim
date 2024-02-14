267b200e4f6e115859e2e9771a2f2036642d96d3
RetoranPetra <flyro@live.co.uk>
2024-02-14 16:00:31 +0000

    Added and configured lua_ls lsp.

--- lua/retoran/plugins/lsp/lspconfig.lua
+++ lua/retoran/plugins/lsp/lspconfig.lua
@@ -4,7 +4,25 @@ return {
 		"williamboman/mason-lspconfig.nvim",
 		dependencies = "williamboman/mason.nvim"
 	},
-	opts = {},
+	opts = {
+		lua_ls = {
+			settings = {
+				Lua = {
+					runtime = { version = "LuaJIT" },
+					diagnostics = {
+						globals = {
+							"vim",
+							"require"
+						},
+					},
+					workspace = {
+						library = vim.api.nvim_get_runtime_file("", true)
+					},
+					telemetry = { enable = false },
+				},
+			},
+		},
+	},
 	config = function(_, opts)
 		-- Enable keybindings
 
@@ -41,20 +59,16 @@ return {
 
 		-- Use masonlspconfig to automatically setup our servers.
 		-- See :h mason-lspconfig-automatic-server-setup
-    require("mason-lspconfig").setup_handlers {
-        -- The first entry (without a key) will be the default handler
-        -- and will be called for each installed server that doesn't have
-        -- a dedicated handler.
-        function (server_name) -- default handler (optional)
-            require("lspconfig")[server_name].setup {}
-        end,
-        -- Next, you can provide a dedicated handler for specific servers.
-        -- For example, a handler override for the `rust_analyzer`:
-        --["rust_analyzer"] = function ()
-        --    require("rust-tools").setup {}
-        --end
-    }
-
-		
+		require("mason-lspconfig").setup_handlers {
+			-- The first entry (without a key) will be the default handler
+			-- and will be called for each installed server that doesn't have
+			-- a dedicated handler.
+			function(server_name)    -- default handler (optional)
+				require("lspconfig")[server_name].setup {}
+			end,
+			["lua_ls"] = function()
+				require("lspconfig")["lua_ls"].setup(opts.lua_ls)
+			end
+		}
 	end
 }
