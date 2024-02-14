a1deebca75799281523e4d5cc007a2e2d3fd4fcb
RetoranPetra <flyro@live.co.uk>
2024-02-14 16:42:14 +0000

    Fixed fzf telescope to use cmake.

--- lua/retoran/plugins/telescope.lua
+++ lua/retoran/plugins/telescope.lua
@@ -1,10 +1,11 @@
 -- change some telescope options and a keymap to browse plugin files
 return {
 	"nvim-telescope/telescope.nvim",
+	opts = {},
 	dependencies = {
 		{
 			"nvim-telescope/telescope-fzf-native.nvim",
-			build = "make",
+			build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build',
 			config = function()
 				require("telescope").load_extension("fzf")
 			end,
