7cca8779155c696784c8ceb771753b853a1b0e11
RetoranPetra <flyro@live.co.uk>
2024-02-14 16:00:28 +0000

    Updated plugins, added a markdown previewer. Not working yet.

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,10 +2,13 @@
 return {
 	"nvim-telescope/telescope.nvim",
 	dependencies = {
-		"nvim-telescope/telescope-fzf-native.nvim",
-		build = "make",
-		config = function()
-			require("telescope").load_extension("fzf")
-		end,
+		{
+			"nvim-telescope/telescope-fzf-native.nvim",
+			build = "make",
+			config = function()
+				require("telescope").load_extension("fzf")
+			end,
+		},
+		{ "nvim-lua/plenary.nvim" },
 	},
 }
