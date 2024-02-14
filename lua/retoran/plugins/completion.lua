6bcd59c9ebf9c7d087d39f8eeacff4a21b7a7f2d
RetoranPetra <flyro@live.co.uk>
2024-02-14 16:42:12 +0000

    Added nvim-lspconfig to dependencies

--- lua/retoran/plugins/completion.lua
+++ lua/retoran/plugins/completion.lua
@@ -5,7 +5,7 @@ return {
 		dependencies = {
 			"f3fora/cmp-spell",
 			"L3MON4D3/LuaSnip",
-			"hrsh7th/cmp-nvim-lsp",
+			{"hrsh7th/cmp-nvim-lsp", dependencies = "neovim/nvim-lspconfig"},
 			"hrsh7th/cmp-buffer",
 			"hrsh7th/cmp-path",
 			"hrsh7th/cmp-cmdline",
