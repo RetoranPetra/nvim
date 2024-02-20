74f28487b2c5d036577521d2fcb84057812a1dae
RetoranPetra <flyro@live.co.uk>
2024-02-20 10:39:34 +0000

    feat: Only open nvim-tree when opening directory

--- lua/retoran/plugins/nvim-tree.lua
+++ lua/retoran/plugins/nvim-tree.lua
@@ -11,8 +11,19 @@ return {
 	},
 	-- Takes opts from above.
 	config = function(_, opts)
+		local function open_nvim_tree(data)
+			local directory = vim.fn.isdirectory(data.file) == 1
+			if not directory then
+				return
+			else
+				vim.cmd.cd(data.file)
+			end
+			require("nvim-tree.api").tree.toggle({ focus = false, find_file = true })
+		end
+
+		vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
+
 		require("nvim-tree").setup(opts)
-		require("nvim-tree.api").tree.toggle({ focus = false, find_file = true })
 	end,
 	event = "VimEnter"
 }
