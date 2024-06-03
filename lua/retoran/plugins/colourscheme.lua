515ace8a1c2aa1bda2b92f500121fcb6856b8fbd
RetoranPetra <flyro@live.co.uk>
2024-06-03 17:21:44 +0100

    feat: add more colorscheme options

--- lua/retoran/plugins/colourscheme.lua
+++ lua/retoran/plugins/colourscheme.lua
@@ -1,12 +1,33 @@
 return {
-	"bluz71/vim-nightfly-colors",
-	name = "nightfly",
-	config = function()
-		if vim.g.usingGUI == nil then
-			vim.g.nightflyTransparent = true
-		end
-		vim.g.nightflyCursorColor = true
-		vim.g.nightflyItalics = true
-		vim.cmd([[colorscheme nightfly]])
-	end,
+	{
+		"bluz71/vim-nightfly-colors",
+		name = "nightfly",
+		lazy = false,
+		priority = 1000,
+		opts = {
+			setOnStart = true,
+		},
+		config = function(_, opts)
+			if vim.g.usingGUI == nil then
+				vim.g.nightflyTransparent = true
+			end
+			vim.g.nightflyCursorColor = true
+			vim.g.nightflyItalics = true
+			if opts.setOnStart then
+				vim.cmd([[colorscheme nightfly]])
+			end
+		end,
+	},
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		opts = {
+			setOnStart = false,
+		},
+		config = function(_, opts)
+			if opts.setOnStart then
+				vim.cmd([[colorscheme tokyonight]])
+			end
+		end,
+	},
 }
