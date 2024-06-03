6f014b591397a2324413615be3fd86058d54bc31
RetoranPetra <flyro@live.co.uk>
2024-06-03 18:11:29 +0100

    refactor: use toggleterm for lazygit

--- lua/retoran/plugins/toggleterm.lua
+++ lua/retoran/plugins/toggleterm.lua
@@ -1,7 +1,40 @@
 return {
 	{
-		'akinsho/toggleterm.nvim',
+		"akinsho/toggleterm.nvim",
 		opts = {},
-		config = true,
-	}
+		config = function(_, opts)
+			local toggleterm = require("toggleterm")
+			local terminal = require("toggleterm.terminal").Terminal
+			toggleterm.setup(opts)
+
+			-- Custom terminals
+			local lazygit = terminal:new({
+				cmd = "lazygit",
+				dir = "git_dir",
+				hidden = true,
+				direction = "float",
+				on_open = function(term)
+					vim.cmd("startinsert!")
+					vim.api.nvim_buf_set_keymap(
+						term.bufnr,
+						"n",
+						"q",
+						"<cmd>close<CR>",
+						{ noremap = true, silent = true }
+					)
+				end,
+			})
+			-- Global
+			function _Lazygit_toggle()
+				lazygit:toggle()
+			end
+
+			vim.api.nvim_set_keymap(
+				"n",
+				"<leader>g",
+				"<cmd>lua _Lazygit_toggle()<CR>",
+				{ noremap = true, silent = true }
+			)
+		end,
+	},
 }
