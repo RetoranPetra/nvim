ebfcfb09e820fcef46374f10841d5708cd7c9c0e
RetoranPetra <flyro@live.co.uk>
2024-06-14 12:01:39 +0100

    Revert "refactor: swap neodev for lazydev"
    This reverts commit c9114d4b4084e17874c95272436faac3d6b418a2.

--- lua/retoran/plugins/completion.lua
+++ lua/retoran/plugins/completion.lua
@@ -12,7 +12,6 @@ return {
 			{ "saadparwaiz1/cmp_luasnip", dependencies = "L3MON4D3/LuaSnip" },
 			"onsails/lspkind.nvim",
 			"windwp/nvim-autopairs",
-			"folke/lazydev.nvim",
 		},
 		lazy = false,
 		opts = function(_, opts)
@@ -40,7 +39,6 @@ return {
 			opts.sources = cmp.config.sources({
 				{ name = "luasnip", priority = 2 },
 				{ name = "nvim_lsp", priority = 1 },
-				{ name = "lazydev", priority = 0 },
 			}, {
 				{ name = "buffer" },
 				{ name = "path" },
