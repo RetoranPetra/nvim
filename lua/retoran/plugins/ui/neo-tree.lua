060c6598b9cec3ec9ea77e93160d6a44a93bd975
RetoranPetra <flyro@live.co.uk>
2025-01-24 13:41:21 +0000

    fix: neo-tree starts correctly in current directory when opening a directory with nvim $DIR

--- lua/retoran/plugins/ui/neo-tree.lua
+++ lua/retoran/plugins/ui/neo-tree.lua
@@ -14,6 +14,10 @@ return {
 		hide_root_node = true,
 		open_files_do_not_replace_types = {},
 		filesystem = {
+			cwd_target = {
+				-- fixes issue where directory chosen with nvim $DIR doesn't have neo-tree start in that directory.
+				current = "global",
+			},
 			follow_current_file = {
 				enabled = true,
 				leave_dirs_open = false,
