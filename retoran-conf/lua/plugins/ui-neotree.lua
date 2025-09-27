3901a3ac097c05a84590445eb298beb25c9428b9
RetoranPetra <flyro@live.co.uk>
2025-09-27 20:36:02 +0100

    wip

---
+++ retoran-conf/lua/plugins/ui-neotree.lua
@@ -0,0 +1,43 @@
+return {
+	"neo-tree.nvim",
+	beforeall = function()
+		vim.g.loaded_netrw = 1
+		vim.g.loaded_netrwPlugin = 1
+	end,
+	after = function()
+		local neotree = require("neo-tree")
+		neotree.setup({
+			hide_root_node = true,
+			open_files_do_not_replace_types = {},
+			filesystem = {
+				cwd_target = {
+					current = "global",
+				},
+				follow_current_file = {
+					enabled = true,
+					leave_dirs_open = false,
+				},
+				hijack_netrw_behaviour = "open_default",
+				use_libuv_file_watcher = true,
+			},
+			buffers = {
+				follow_current_file = {
+					enabled = true,
+					leave_dirs_open = false,
+				},
+			},
+			window = {
+				position = "current",
+			},
+		});
+		vim.keymap.set("n", "<leader>z", [[:Neotree buffers position=float<CR>]], {
+			silent = true,
+			desc = "Neotree buffers"
+		})
+		vim.keymap.set("n", "<leader>x", [[:Neotree toggle position=float<CR>]], {
+			silent = true,
+			desc = "Neotree"
+		})
+	end,
+	lazy = false,
+}
