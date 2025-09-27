45a72abb416650c9de5c7ea87e22198f19924ac3
RetoranPetra <flyro@live.co.uk>
2025-09-27 22:52:35 +0100

    add lualine options

--- config/ui.nix
+++ config/ui.nix
@@ -2,7 +2,15 @@
   plugins = {
     bufferline.enable = true;
     dressing.enable = true;
-    lualine.enable = true;
+    lualine = {
+      enable = true;
+      settings = {
+        options = {
+          icons_enabled = true;
+          theme = "auto";
+        };
+      };
+    };
     notify.enable = true;
     noice.enable = true;
     web-devicons.enable = true;
