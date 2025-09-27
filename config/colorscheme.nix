166d4d4692bbd4f5103c5f5f2d9cc39f154e5d7f
RetoranPetra <flyro@live.co.uk>
2025-09-27 23:31:15 +0100

    colorscheme working

--- config/colorscheme.nix
+++ config/colorscheme.nix
@@ -1,4 +1,10 @@
 { pkgs, ... }:
 {
   extraPlugins = [pkgs.vimPlugins.nightfly];
+  globals = {
+    nightflyTransparent = true;
+    nightflyItalics = true;
+    nightflyCursorColor = true;
+  };
+  colorscheme = "nightfly";
 }
