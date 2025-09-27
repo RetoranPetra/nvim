e048b3800625dc2b31130116b16863ef3e717900
RetoranPetra <flyro@live.co.uk>
2025-09-27 16:48:57 +0100

    packpath stuff

--- neovim.nix
+++ neovim.nix
@@ -2,14 +2,38 @@
 	symlinkJoin,
 	neovim-unwrapped,
 	makeWrapper,
+	runCommandLocal,
+	vimPlugins,
+	lib,
 }:
-symlinkJoin {
-	name = "neovim-custom";
-	paths = [neovim-unwrapped];
-	nativeBuildInputs = [makeWrapper];
-	postBuild = ''
-		wrapProgram $out/bin/nvim \
-		--add-flags '-u' \
-		--add-flags '${./init.lua}'
+let
+	packageName = "mypackage";
+
+	startPlugins = [
+		vimPlugins.plenary-nvim
+		vimPlugins.telescope-nvim
+	];
+	packpath = runCommandLocal "packpath" {} ''
+		mkdir -p $out/pack/${packageName}/{start,opt}
+
+		${
+		lib.concatMapStringsSep
+		"\n"
+		(plugin: "ln -vsfT ${plugin} $out/pack/${packageName}/start/${lib.getName plugin}")
+		startPlugins
+	}
 	'';
-}
+in
+	symlinkJoin {
+		name = "neovim-custom";
+		paths = [neovim-unwrapped];
+		nativeBuildInputs = [makeWrapper];
+		postBuild = ''
+			wrapProgram $out/bin/nvim \
+			--add-flags '-u' \
+			--add-flags '${./init.lua}'
+		'';
+		passthru = {
+			inherit packpath;
+		};
+	}
