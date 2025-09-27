2a983babc2e7fd3f10ecd5663f94a2eb519b07bb
RetoranPetra <flyro@live.co.uk>
2025-09-27 16:38:23 +0100

    bring back some plugins

---
+++ lua/retoran/core/defaults.lua
@@ -0,0 +1,8 @@
+local opt = vim.opt
+opt.timeoutlen = 1500
+opt.undofile = true
+opt.undolevels = 10000
+opt.termguicolors = true
+opt.swapfile = false
+-- Polyglot wants this
+opt.compatible = false
---
+++ lua/retoran/core/fileTypeOverride.lua
@@ -0,0 +1,13 @@
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+	desc = "Set filetype for xml-like files without types to xml.",
+	pattern = {
+		"*.xaml",
+		"*.axaml",
+	},
+	command = "set filetype=xml",
+})
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+	desc = "Set filetype for nuget cache (Pointless but neat)",
+	pattern = { "*.nuget.cache" },
+	command = "set filetype=json",
+})
