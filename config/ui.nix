1749fd5c1944fbe413f362febcf86eb1b4cfe5d3
RetoranPetra <flyro@live.co.uk>
2025-09-28 10:18:07 +0100

    neo-tree config ported

--- config/ui.nix
+++ config/ui.nix
@@ -1,7 +1,37 @@
 {
+  # Need to disable netrw for neo-tree
+  globals = {
+    loaded_netrw = 1;
+    loaded_netrwPlugin = 1;
+  };
+
   plugins = {
     bufferline.enable = true;
     dressing.enable = true;
+    neo-tree = {
+      enable = true;
+      extraOptions = {
+        hide_root_node = true;
+        filesystem = {
+          cwd_target.current = "global";
+          follow_current_file = {
+            enabled = true;
+            leave_dirs_open = false;
+          };
+          hijack_netrw_behaviour = "open_default";
+          use_libuv_file_watcher = true;
+        };
+        buffers = {
+          follow_current_file = {
+            enable = true;
+            leave_dirs_open = false;
+          };
+        };
+        window = {
+          position = "current";
+        };
+      };
+    };
     lualine = {
       enable = true;
       settings = {
