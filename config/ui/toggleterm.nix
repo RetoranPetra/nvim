401e73493a8cd289ebac4e82695c09b93a583f31
RetoranPetra <flyro@live.co.uk>
2025-09-28 15:45:36 +0100

    move lua code to lua files

--- config/ui/toggleterm.nix
+++ config/ui/toggleterm.nix
@@ -10,27 +10,7 @@
       open_mapping = "[[<leader>t]]";
     };
   };
-  extraConfigLua = ''
-    local toggleterm = require("toggleterm")
-    local terminal = require("toggleterm.terminal").Terminal
-
-    local floatTerm = terminal:new({
-      hidden = true,
-      direction = "float",
-      on_open = function(term)
-        if term.is_float(term) then
-          vim.cmd("startinsert!")
-        end
-        vim.api.nvim_buf_set_keymap(term.bufnr, "n", "<esc>", "<cmd>close<CR>", {
-          silent = true,
-        })
-      end,
-    })
-
-    function _FloatTerm_toggle()
-      floatTerm:toggle()
-    end
-  '';
+  extraConfigLua = builtins.readFile ./toggleterm-extraConfigLua.lua;
   keymaps = [
     {
       mode = "n";
