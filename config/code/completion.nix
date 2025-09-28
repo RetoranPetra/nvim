6d209d66f2051e019134da3b71b69c46f7ac8b40
RetoranPetra <flyro@live.co.uk>
2025-09-28 17:20:08 +0100

    completion with luasnip

--- config/code/completion.nix
+++ config/code/completion.nix
@@ -8,14 +8,19 @@
       enable = true;
       settings = {
         sources = [
-          [
+            { name = "luasnip"; }
             { name = "nvim_lsp"; }
-          ]
-          [
             { name = "buffer"; }
             { name = "path"; }
-          ]
         ];
+        snippet = {
+          expand =
+            ''
+            function(args)
+              require"luasnip".lsp_expand(args.body)
+            end
+            '';
+        };
       };
     };
   };
--- config/code/default.nix
+++ config/code/default.nix
@@ -1,5 +1,6 @@
 {
   imports = [
     ./lsp.nix
+    ./completion.nix
   ];
 }
