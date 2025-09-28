b7bfd41c8121d980102ad10014b0292fde455ffe
RetoranPetra <flyro@live.co.uk>
2025-09-28 18:11:39 +0100

    set groups for completion

--- config/code/completion.nix
+++ config/code/completion.nix
@@ -12,10 +12,10 @@
       enable = true;
       settings = {
         sources = [
-            { name = "luasnip"; }
-            { name = "nvim_lsp"; }
-            { name = "buffer"; }
-            { name = "path"; }
+            { name = "luasnip"; group_index = 1; }
+            { name = "nvim_lsp"; group_index = 1; }
+            { name = "buffer"; group_index = 2; }
+            { name = "path"; group_index = 2; }
         ];
         preselect = "require'cmp'.PreselectMode.None";
         # NOTE: formatting.format is set in lspkind config, we don't need it here.
