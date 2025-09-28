850e448aad325d9309111b00bbc7fa3ff1dc3555
RetoranPetra <flyro@live.co.uk>
2025-09-28 12:02:45 +0100

    add more highlighting

--- config/highlighting/default.nix
+++ config/highlighting/default.nix
@@ -2,5 +2,8 @@
   imports = [
     ./colouriser.nix
     ./indentscope.nix
+    ./todo.nix
+    ./treesitter.nix
+    ./polyglot.nix
   ];
 }
---
+++ config/highlighting/polyglot.nix
@@ -0,0 +1,4 @@
+# Need to adapt over that bit of code that enables parsers that aren't in nvim-treesitter
+{
+
+}
