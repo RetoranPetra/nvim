a8c30f044dfe2514546f47362ff6aff6c1057c3e
RetoranPetra <flyro@live.co.uk>
2025-10-02 19:42:40 +0100

    use all 25.05 packages

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
-    nixvim.url = "github:nix-community/nixvim";
+    nixvim.url = "github:nix-community/nixvim/nixos-25.05";
     flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
